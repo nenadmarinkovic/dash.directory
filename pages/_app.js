9808f7af13164897a29017ae3f1830ac52036004
nenadmarinkovic <nenadmarinkovic@protonmail.com>
2023-04-12 17:10:51 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+// import { useEffect } from "react";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "../styles/global";
 import { useTheme } from "../hooks/useTheme";
@@ -9,11 +9,11 @@ export default function App({ Component, pageProps }) {
   const [theme, toggleTheme, componentMounted] = useTheme();
   const themeMode = theme === "light" ? lightTheme : darkTheme;
 
-  useEffect(() => {
-    navigator.serviceWorker
-      .register("/sw.js")
-      .catch((err) => console.log("Service Worker registration failed: ", err));
-  }, []);
+  // useEffect(() => {
+  //   navigator.serviceWorker
+  //     .register("/sw.js")
+  //     .catch((err) => console.log("Service Worker registration failed: ", err));
+  // }, []);
 
   if (!componentMounted) {
     return <div />;
